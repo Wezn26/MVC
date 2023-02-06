eadf8c4a817275e2c0020033564078a67e95b219
Leonid Second Comp <li_che_in@mail.ru>
2023-02-06 13:52:26 +0300

    Start lesson7

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -11,11 +11,22 @@ abstract class Controller
 {
     public $route;
     public $view;
+    public $model;
     
     public function __construct($route) 
     {
         $this->route = $route;
         $this->view = new \App\core\View($route);
+        $this->model = $this->loadModel($route['controller']); 
+        
         //$this->before();
+    }
+    
+    public function loadModel($name) {
+        $path = 'App/Models/' . ucfirst($name);
+        if (class_exists($path)) {
+            return new $path;
+        }
+        //var_dump($path);
     }
 }
